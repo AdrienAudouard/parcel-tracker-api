0e767c2257ee11ab34138a42feb606f876831da3
adrien.audouard@gmail.com <adrien.audouard@gmail.com>
2020-05-01 15:16:22 +0200

    add keyword

--- sandbox/client.js
+++ sandbox/client.js
@@ -3,8 +3,8 @@ const Tracker = require('../dist/index').default;
 const config = {
     enableCrossOrigin: false,
     apiKeys: {
-        laPoste: 'Q3kDbaq+r73FmR+FGP9kP+MhKQQ10z5F34tcQBQc0dlhJIb2WJ8Tnt7OB7NL/A4C',
-        DHL: 'yG3nXMO0PYqJ9ZDVyzieWotnPJHuCEQi'
+        laPoste: '',
+        DHL: ''
     }
 };
 
